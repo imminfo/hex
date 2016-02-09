03c24d2b99d602d5ac73f84c19469c5a1c576fc6
Vadim N <vdn@mailbox.org>
2016-02-09 16:37:08 +0300

    N- and H- clusterisation

--- clusterisation.py
+++ clusterisation.py
@@ -61,14 +61,15 @@ def classify(fqdata, low_threshold, high_threshold):
     else:
         return 0
 
-
-def classify_fastq_file(filepath, replace_threshold, low_threshold, high_threshold):
-    def _stats(cls_seq, which_class):
-        print("\t", which_class, " seq:\t", len(cls_seq[which_class]), sep = "", end = "")
-        print("(", round(len(cls_seq[which_class]) / sum(map(len, cls_seq)), 4), ")", sep = "")
-        print("\t", which_class, " rds:\t", sum(cls_seq[which_class].values()), sep = "", end = "")
-        print("(", round(len(cls_seq[which_class]) / sum(map(lambda x: sum(x.values()), cls_seq)), 4), ")", sep = "")
     
+def cls_stats(cls_seq, which_class):
+    print("\t", which_class, " seq:\t", len(cls_seq[which_class]), sep = "", end = "")
+    print("(", round(len(cls_seq[which_class]) / sum(map(len, cls_seq)), 4), ")", sep = "")
+    print("\t", which_class, " rds:\t", sum(cls_seq[which_class].values()), sep = "", end = "")
+    print("(", round(len(cls_seq[which_class]) / sum(map(lambda x: sum(x.values()), cls_seq)), 4), ")", sep = "")
+        
+
+def classify_fastq_file(filepath, replace_threshold, low_threshold, high_threshold):    
     print("Fastq records:\t", int(num_lines(filepath) / 4), sep = "")
     
     cls_seq = [{}, {}, {}]
@@ -77,9 +78,9 @@ def classify_fastq_file(filepath, replace_threshold, low_threshold, high_thresho
         cls = classify(fqdata, low_threshold, high_threshold)
         cls_seq[cls][fqdata.seq] = cls_seq[cls].get(fqdata.seq, 0) + 1
     print("Classes:")
-    _stats(cls_seq, 0)
-    _stats(cls_seq, 1)
-    _stats(cls_seq, 2)
+    cls_stats(cls_seq, 0)
+    cls_stats(cls_seq, 1)
+    cls_stats(cls_seq, 2)
     print()
     return cls_seq
 
@@ -90,96 +91,91 @@ def X_clust(minors, majors, x_clust_hamm):
     H-clust: merge medium quality sequences with the high quality ones.
     """
     
-    keys = list(seq_dict.keys())
-    key_ind = dict([(keys[i], i) for i in range(len(keys))])
+    def _get_keys(seq_dict):
+        keys = list(seq_dict.keys())
+        key_ind = dict([(keys[i], i) for i in range(len(keys))])
+        return keys, key_ind
+        
     cands = {}
     merged_seq = {x: set([]) for x in majors}
+    minor_keys, minor_key_inds = _get_keys(minors)
+    major_keys, major_keys_inds = _get_keys(majors)
     
-    for i in range(len(seq_dict) - 1):
-        for j in range(i + 1, len(seq_dict)):
-            if hamm(keys[i], keys[j], x_clust_hamm):
-                key = i
-                append_to = j
-                if seq_dict[keys[i]] > seq_dict[keys[j]]:
-                    key = j
-                    append_to = i
+    
+    print("Computing distances...")
+    for i in range(len(minors)):
+        for j in range(len(majors)):
+            if hamm(minor_keys[i], major_keys[j], x_clust_hamm):
+                if i not in cands: cands[i] = []
+                cands[i].append(j)
 
-                if key not in cands: cands[key] = []
-                cands[key].append(append_to)
+                
+    print("Merging sequences...")
+    for minor_key, targets in cands.items():
+        for target in targets:
+            merged_seq[major_keys[target]].add((minor_key, minors[minor_keys[minor_key]] / len(targets)))
 
-    while cands:
-        for seq, cnt in sorted(seq_dict.items(), key = lambda x: x[1]):
-            if key_ind[seq] in cands:
-                for append_to in cands[key_ind[seq]]:
-                    # seq_dict[keys[append_to]] += math.ceil(seq_dict[seq] / len(cands[key_ind[seq]]))
-                    seq_dict[keys[append_to]] += seq_dict[seq] / len(cands[key_ind[seq]])
-                    if merged_seq[seq]:
-                        merged_seq[keys[append_to]] = merged_seq[keys[append_to]].union(merged_seq[seq])
-                    # merged_seq[keys[append_to]].add((key_ind[seq], math.ceil(seq_dict[seq] / len(cands[key_ind[seq]]))))
-                    merged_seq[keys[append_to]].add((key_ind[seq], seq_dict[seq] / len(cands[key_ind[seq]])))
-                seq_dict[seq] = 0
-
-        to_pop = set([x for x in cands])
-        for source in cands:
-            for to_append in cands[source]:
-                if seq_dict[keys[source]] and source in to_pop:
-                    to_pop.remove(source)
-
-        for p in to_pop:
-            cands.pop(p)
-            merged_seq.pop(keys[p])
-            seq_dict.pop(keys[p])
-
-
+    new_minors = {x: minors[x] for x in minor_keys if x not in cands}
+            
     print("Making consensuses...")
-
-    print("sum pre merging", sum(seq_dict.values()))
+    sum_pre = sum(majors.values())
     n_merged = 0
     new_seq_dict = {}
     for seq, seq_ls in merged_seq.items():
-        new_seq = make_best_pos_consensus(seq, seq_dict[seq], [(keys[x[0]], x[1]) for x in seq_ls])
+        new_seq = make_best_pos_consensus(seq, majors[seq], [(minor_keys[x[0]], x[1]) for x in seq_ls])
         if new_seq not in new_seq_dict:
             new_seq_dict[new_seq] = 0
         else:
             n_merged += 1
-        new_seq_dict[new_seq] += seq_dict[seq]
+        new_seq_dict[new_seq] += majors[seq]
 
     print("# merged:", n_merged)
-    print("sum post merging", sum(new_seq_dict.values()))
-    return new_seq_dict
+    sum_post = sum(new_seq_dict.values())
+    if sum_pre != sum_post:
+        print("Sums are not equal!", sum_pre, "vs.", sum_post)
+    return new_minors, new_seq_dict
 
 
-def clusterise_sequences(f1, replace_threshold, low_threshold, high_threshold, out_seq = "tmp.topseq1.txt", out_blast = "tmp.blast1.txt"):
+def write_and_blast(seq_dict, f1, out_seq, out_blast, max_sequences):
+    with open(out_seq, 'w') as file:
+        i = 0
+        for key, val in reversed(sorted(seq_dict.items(), key = lambda x: x[1])):
+            # print(val, " (", round(100 * val / sum(final_seq.values()), 4), "%)", sep = '')
+            print(val, key, sep = '\t', file = file)
+            i += 1
+            if i == max_sequences: break
+                
+    ls = []
+    i = 0
+    for key, val in reversed(sorted(seq_dict.items(), key = lambda x: x[1])):
+        ls.append(faseq(name = "sequence" + str(i) + "_" + str(val) + "_(" + str(round(100 * val / sum(seq_dict.values()), 4)) + ")", seq = key, comm = ''))
+        i += 1
+        if i == max_sequences: break
+    write_fasta(f1 + ".seq.fasta.txt", ls)
+    os.system("blastn -query " + f1 + ".seq.fasta.txt" + " -db hlabase/hlabase.fasta -outfmt 6 -num_alignments 4 > " + out_blast)
+
+
+def clusterise_sequences(f1, replace_threshold, low_threshold, high_threshold, n_clust_hamm, h_clust_hamm, max_sequences, out_seq_prefix = "tmp.topseq1", out_blast_prefix = "tmp.blast1"):
     prefix = f1[:f1.find(".fastq")]
-    print("Searching for unique sequences..."); d1 = {}
+    
+    print("Searching for unique sequences...")
+    cls_seq = classify_fastq_file(f1, replace_threshold, low_threshold, high_threshold)
 
-    classify_fastq_file(f1, replace_threshold, low_threshold, high_threshold)
-
-
-    print("Clustering error sequences...")
-#     d1 = merge_with_clusters(d1, seq_threshold, qmed_threshold)
-
-
-#     print("Writing results...")
-
-#     with open(out_seq, 'w') as file:
-#         i = 0
-#         for key, val in reversed(sorted(d1.items(), key = lambda x: x[1])):
-#             # print(val, " (", round(100 * val / sum(d1.values()), 4), "%)", sep = '')
-#             print(val, key, sep = '\t', file = file)
-#             i += 1
-#             if i == max_sequences: break
-#             # if val < 2: break
-
-#     ls = []
-#     i = 0
-#     for key, val in reversed(sorted(d1.items(), key = lambda x: x[1])):
-#         ls.append(faseq(name = "sequence" + str(i) + "_" + str(val) + "_(" + str(round(100 * val / sum(d1.values()), 4)) + ")", seq = key, comm = ''))
-#         i += 1
-#         if i == max_sequences: break
-#     write_fasta(f1 + ".seq.fasta.txt", ls)
-#     os.system("blastn -query " + f1 + ".seq.fasta.txt" + " -db hlabase/hlabase.fasta -outfmt 6 -num_alignments 4 > " + out_blast)
-
+    
+    print("Clustering error sequences...")    
+    _, cls_seq[1] = X_clust(cls_seq[0], cls_seq[1], n_clust_hamm)
+    cls_seq[1], cls_seq[2] = X_clust(cls_seq[1], cls_seq[2], h_clust_hamm)
+    
+    
+    print("Final statistics by class:")
+    cls_stats(cls_seq, 1)
+    cls_stats(cls_seq, 2)
+    print()
+    
+    print("Writing results...")
+    write_and_blast(cls_seq[1], f1 + ".minor", out_seq_prefix + ".minor.txt", out_blast_prefix + ".minor.txt", max_sequences)
+    write_and_blast(cls_seq[2], f1 + ".major", out_seq_prefix + ".major.txt", out_blast_prefix + ".major.txt", max_sequences)
+    
 
 if __name__ == '__main__':
     aggregate_sequences(sys.argv[1], 50, 7, 5, "tmp.topseq1.txt", "tmp.blast1.txt")
